ba4865e239f93db9cdb07c02b56d5aca7bd34bfe
Paul Tagliamonte <paultag@sunlightfoundation.com>
2013-04-12 13:08:30 -0400

    Adding more test coverage.

--- tests/validation/test_address.py
+++ tests/validation/test_address.py
@@ -1,6 +1,12 @@
 from popolo.address import PopoloAddress
 
 
+_TEST_ADDRESS = """
+20700 North Park Blvd,
+University Heights, Ohio
+44118"""
+
+
 def test_invalid_address_entry():
     """ Test to make sure we can't make empty addresses. """
     try:
@@ -12,3 +18,4 @@ def test_invalid_address_entry():
 def test_valid_address_entry():
     """ Test to make sure we can create an address """
     address = PopoloAddress("guid", 'capitol', fax="Who the hell uses a fax")
+    address = PopoloAddress("guid", 'capitol', address=_TEST_ADDRESS)
