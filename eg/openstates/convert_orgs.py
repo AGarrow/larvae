d984d5ab7cd361c468eef89cca53b170833b9a1e
Paul Tagliamonte <paultag@sunlightfoundation.com>
2013-04-18 13:26:12 -0400

    sketching out a converter for committees

---
+++ eg/openstates/convert_orgs.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+from popolo.organization import Organization
+from popolo.membership import Membership
+from billy.core import db
+
+
+def save_objects(payload):
+    pass
+
+
+def save_object(payload):
+    return save_objects([payload])
+
+
+def create_committee_orgs():
+    for committee in db.committees.find():
+        orga = Organization(committee['_id'],
+                            committee['committee'])
+        memberships = []
+        # XXX: Fix how we do subcommittees. Pass one to get all committees,
+        # then pass over again and assoc as members?
+
+        for member in committee['members']:
+            if not member.get("leg_id", None):
+                continue  # XXX: Expected behavior?
+
+            person_id = member["leg_id"]
+            cid = committee['_id']
+            guid = "{cid}.{person_id}".format(**locals())
+            memberships.append(Membership(guid, person_id, orga.id))
+
+        save_objects(memberships)
+        save_object(committee)
+
+if __name__ == "__main__":
+    create_committee_orgs()
